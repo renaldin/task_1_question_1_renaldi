adf70a7a1417217d73e22497ba7b03783079230b
renaldin1 <renaldinoviandi0@gmail.com>
2025-05-07 07:46:53 +0700

    Adjust

--- task1.ts
+++ task1.ts
@@ -1,26 +1,31 @@
-let current = 1
-let looping = 1
-while (current <= 100) {
-  console.info(`Perulangan 10 ke-${looping}:`)
-  let count = 0
-  let sum = 0
-  while (count < 5 && current <= 100) {
-    if (looping % 2 !== 0) {
-      if (current % 2 !== 0) {
-        console.info(current)
-        sum += current
-        count++
-      }
-    } else {
-      if (current % 2 === 0) {
-        console.info(current)
-        sum += current
-        count++
-      }
+function outputNumbers(start: number, end: number, even: boolean): void {
+  for (let i = start; i <= end; i++) {
+    if ((i % 2 === 0 && even) || (i % 2 !== 0 && !even)) {
+      console.info(i)
     }
-    current++
   }
+}
+
+function sum(start: number, end: number, even: boolean): number {
+  let sum = 0
+  for (let i = start; i <= end; i++) {
+    if ((i % 2 === 0 && even) || (i % 2 !== 0 && !even)) {
+      sum += i
+    }
+  }
+  return sum
+}
+
+const totalLoops = 10
+let totalSum = 0
+for (let i = 1; i <= totalLoops; i++) {
+  const start = (i - 1) * 10 + 1
+  const end = i * 10
+  const even = i % 2 === 0
+  console.info(`Perulangan 10 ke-${i}:`)
+  outputNumbers(start, end, even)
   console.info('--------------------------')
-  console.info(`Jumlah: ${sum}`)
-  looping++
+  const loopSum = sum(start, end, even)
+  console.info(`Jumlah: ${loopSum}\n`)
+  totalSum += loopSum
 }
